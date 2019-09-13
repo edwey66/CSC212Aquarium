ffbf6ccda2d05707189e3f903d3facc0ac7dfc89
edweyzarodriguez <edweyzarodriguez@131.229.155.151>
2019-09-13 16:29:32 -0400

    added fish class

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -43,12 +43,17 @@ public class Aquarium extends GFX {
 
 	int fish1X = getWidth() + 100;
 	int fish2X = getWidth() + 300;
-
+	int fish3X = getWidth() + 250;
+	Fish nemo = new Fish(Color.red,250,250);
+	Fish dory = new Fish(Color.cyan,100,100);
 	@Override
 	public void draw(Graphics2D g) {
 		// Draw the "ocean" background.
 		g.setColor(Color.blue);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		 
+		nemo.draw();
+		dory.draw();
 
 		// Draw the fish!
 		DrawFish.facingLeft(g, Color.yellow, fish1X, 200);
@@ -56,7 +61,7 @@ public class Aquarium extends GFX {
 		DrawFish.facingRight(g, Color.green, fish2X, 300);
 
 		// What if we wanted this little fish to swim, too?
-		DrawFish.smallFacingLeft(g, Color.red, 200, 100);
+		DrawFish.smallFacingLeft(g, Color.red, fish3X, 100);
 
 		// Draw our snail!
 		algorithm.draw(g);
@@ -64,6 +69,7 @@ public class Aquarium extends GFX {
 		// Move the fish!
 		fish1X -= 1;
 		fish2X -= 2;
+		fish3X -= 3;
 	}
 
 	public static void main(String[] args) {
